dacf7168c97d29bbb4a9a0528eeef5f48398dc70
Artem Nikiforov <pocoone28@gmail.com>
2022-11-05 20:54:03 +0300

    1.0

---
+++ src/main/java/com/thehecklers/sburrestdemo/SburRestDemoApplication.java
@@ -0,0 +1,42 @@
+package com.thehecklers.sburrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.UUID;
+
+@SpringBootApplication
+public class SburRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SburRestDemoApplication.class, args);
+	}
+
+}
+
+class Coffee {
+	private final String id;
+	private String name;
+
+
+	public Coffee(String id, String name) {
+		this.id = id;
+		this.name = name;
+	}
+
+	public Coffee(String name) {
+		this(UUID.randomUUID().toString(), name);
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
